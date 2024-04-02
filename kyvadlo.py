2c07ea37508ba7f58a623eb26f08e3030ab8b57d
Rskul <richardsku@gmail.com>
2024-04-02 18:38:43 +0200

    m

--- kyvadlo.py
+++ kyvadlo.py
@@ -1,6 +1,15 @@
 # Import numpy
 import numpy as np
+import xml.etree.ElementTree as ET
+
 
 #123456789
 a = 1010
 print (a)
+
+treeDiffVar = ET.parse('measurements_nooutlier_differentvar.xml')
+DataDiffVar = tree.getroot()
+treeSameVar = ET.parse('measurements_nooutlier_differentvar.xml')
+DataSameVar = tree.getroot()
+treeOutlier = ET.parse('measurements_nooutlier_differentvar.xml')
+DataOutlier = tree.getroot()
