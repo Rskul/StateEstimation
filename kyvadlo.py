91a9c9e2e1c8ceb92466e7508526306f3bc6e3bf
Rskul <richardsku@gmail.com>
2024-04-03 22:30:20 +0200

    update

--- kyvadlo.py
+++ kyvadlo.py
@@ -1,5 +1,6 @@
 # Imports
 import numpy as np
+#from numpy.linalg import inv
 import scipy as sp
 from pykalman import KalmanFilter
 import xml.etree.ElementTree as ET
@@ -10,9 +11,9 @@ import xml.etree.ElementTree as ET
 # OUT - dataset containing outliers
 treeDV = ET.parse('measurements_nooutlier_differentvar.xml')
 DataDV = treeDV.getroot()
-treeSV = ET.parse('measurements_nooutlier_differentvar.xml')
+treeSV = ET.parse('measurements_nooutlier_samevar.xml')
 DataSV = treeSV.getroot()
-treeOUT = ET.parse('measurements_nooutlier_differentvar.xml')
+treeOUT = ET.parse('measurements_outlier_samevar.xml')
 DataOUT = treeOUT.getroot()
 
 #Empty array to be used for saving and graphing extimated data
@@ -27,8 +28,9 @@ ts = 0.05
 
 
 # Definition of initial state and initial covariance
-x_init = np.array([0,0])
-P_init = np.array ([1,0],[0,1])
+# Measurement dataSV and dataDV sugests that the pendulum is at maximal displacement so arcsin(1) was chosen as the starting point
+x_init = np.array([np.arcsin(1), 0])
+P_init = np.array([[1, 0],[0, 1]])
 
 
 
@@ -45,10 +47,10 @@ P_init = np.array ([1,0],[0,1])
 #     (-g*ts*cos(a_(k-1)) , 1 )
 # s is cos of predited value of a_(k-1) from previous step
 s = np.cos(x_init[0])
-F = np.array([1,ts],[-g*ts*s,1])
+F = np.array([[1, ts], [-g*ts*s, 1]])
 
 #Process noise
-Q = np.array([q*pow(ts,3)/3,q*pow(ts,2)/2],[q*pow(ts,2)/2,q*ts])
+Q = np.array([[q*pow(ts,3)/3, q*pow(ts,2)/2], [q*pow(ts, 2)/2, q*ts]])
 
 #measurement
 # m = sin (a) + r
@@ -56,7 +58,7 @@ Q = np.array([q*pow(ts,3)/3,q*pow(ts,2)/2],[q*pow(ts,2)/2,q*ts])
 # Here we also linearize the mesurement model
 # H will be the Jakobian for measurment transformation
 # H = (cos(a) , 0)^T
-H = np.array([s,0])
+H = np.array([s, 0])
 
 
 # Kalman filter
@@ -67,41 +69,48 @@ H = np.array([s,0])
 # x_next, P_next, estimated value of x (P) given by kalman filter
 # filling them if empti sets to set correct datatype
 x_apri = x_init
-x_post = np.array([0,0])
-x_next = np.array([0,0])
+x_post = np.array([0, 0])
+x_next = np.array([0, 0])
 P_apri = P_init
-P_post = np.array([1,0],[0,1])
-P_next = np.array([1,0],[0,1])
+P_post = np.array([[1, 0],[0, 1]])
+P_next = np.array([[1, 0],[0, 1]])
 
 # K_gain - Kalman gain
 
-for i in range(100):
-    # preparing linearised system for current step
+for i in range(101):
+    # preparing linearized system for evolution of covariance
     s = np.cos(x_apri[0])
-    F = np.array([1,ts],[-g*ts*s,1])
-    H = np.array([s,0])
+    F = np.array([[1, ts],[-g*ts*s, 1]])
     # measurment noise prom data
-    R = float(DataSV[i][2].text)
+    r = float(DataSV[i][2].text)
+    R = r
     
 
-    # prediction
+    # prediction step
     # based on evolution of x using system state equations
     # linearized evolution of covariance
-    x_post = np.array([x_apri[0] + x_apri[1]*ts, x_apri[1] -g*ts*np.sin(x_apri[0])])
+    x_post = np.array([x_apri[0] + x_apri[1]*ts, x_apri[1] - g*ts*np.sin(x_apri[0])])
     P_post = F.dot(P_apri).dot(F.T) + Q
 
-    # update
+    # update step
+    # updating Jakobian of mesurement predistion
+    s = np.cos(x_post[0])
+    H = np.array([s, 0])
     # Kalman gain computation
-    K = P_post.dot(H.T)*pow(H.dot(P_post).dot(H.T) + R,-1)
+    K = (P_post.dot(H.T)) /(H.dot(P_post).dot(H.T) + R)
     # Measurements m and predicted measurement h
     m = float(DataSV[i][1].text)
-    h = H.dot(x_post)
+    h = np.sin(x_post[0])
+
+    # Estimation step
     # Calculating estimated vaues of x and P with Kalman gain and measurements
-    x_next = x_post + K*(m-h)
-    P_next = (np.array([1,0],[0,1]) - K.dot(H)).dot(P_post)
+    x_next = x_post + K.dot(m-h)
+    P_next = (np.array([[1, 0], [0, 1]]) - K.dot(H)).dot(P_post)
 
     # Print, save and assign results
-    print('time = ', i*ts, ',estimated angle = ', x_next[0], 'estimated angular velocity =', x_next[1], 'estimated displacement = ', H.dot(x_next))
+    print('time = ', round(i*ts,3), '    estimated angle = ', round(x_next[0],3), '    estimated angular velocity =', round(x_next[1],3),'    estimated displacement = ', round(np.sin(x_post[0]),3), '    measured displacement = ',round(m,3))
+    x_apri = x_next
+    P_apri = P_next
 
 
 
