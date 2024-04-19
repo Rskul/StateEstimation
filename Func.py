b56cb0c6cd28ff2dbb90a46f59871559deab6fae
Rskul <richardsku@gmail.com>
2024-04-19 11:20:33 +0200

    Formating and cleanup

---
+++ Func.py
@@ -0,0 +1,90 @@
+import numpy as np
+from numpy.linalg import inv
+import pandas as pd
+
+# Constatns of symple pendulum system
+g = 9.81  # Gravitational constant
+q = 0.1  # Spectral density
+ts = 0.05  # time step (constant in this case)
+
+# Functons for calculation of KF of simple pendulum system
+# x = State of system
+# x is vector with (a, a') where a is angle and a' is angular velocity
+# P = System covariances
+
+
+def KF_xEvol(x):
+    """
+    Calculate Time evolution of simple pendulum system.
+
+    Inputs system state x.
+    Outputs state x after time ts has passed.
+
+    Time evolution according following mathematical model:
+
+    state space model (x_k = f(x_(k-1) + process noise)
+    x = (a,a')
+    a = angle, a' = angular velocity
+    a_k  = a_(k-1) + a'_(k-1) * ts
+    a'_k = a'_(k-1) - g sin (a_(k-1)) *ts.
+
+    both input and output x are vectors (a, a') where a is angle and a' is angular velocity.
+    """
+    return np.array([x[0] + x[1]*ts, x[1] - g*ts*np.sin(x[0])])
+
+
+def EKF_PTransitionMatrix(x):
+    """
+    Calculate linearized transition matrix.
+
+    Inputs is state of system x.
+    Outputs linearized transition matrix time evlution of the system by time ts.
+
+    Linearisation obtained from jacobian of time evolution of system.
+
+    F = df/dx|_(x_k-1) Jakobian derived from f at predicted x_k-1
+    F = (       1           , ts)
+        (-g*ts*cos(a_(k-1)) , 1 )
+    s is cos of predited value of a_(k-1) from previous step
+
+    Input is vector x.
+    Outputs 2x2 matrix.
+    """
+    s = np.cos(float(x[0]))
+    return np.array([[1, ts], [-g*ts*s, 1]])
+
+
+def KF_MeasurementTransformation(x):
+    """
+    Calculate displacement from the angle of pendulum.
+
+    Inputs state of system x.
+    Outputs value of displacement.
+
+    measurement:
+    m = sin(a) + r
+    r = observation noise with variance from dataset at given time.
+
+    Input is vector x.
+    Output is scalar value.
+    """
+    return np.sin(x[0])
+
+
+def EKF_LinearizedMeasurementTransformation(x):
+    """
+    Calculate Linearized calculation of displacement from system state x.
+
+    Inputs state of system x.
+    Outputs transtition matrix that transforms system state values to displacement.
+
+    Linearization is obtained by calculation of Jakobian. 
+
+    H = (cos(a), 0)
+    
+    Input is vector x.
+    Output is 2x1 matrix. (covector)
+    """
+
+    s = np.cos(float(x[0]))
+    return np.array([[s, 0]])  # 2x1 matrix
--- kyvadlo.py
+++ kyvadlo.py
@@ -1,10 +1,16 @@
 # Imports
+import xml.etree.ElementTree as ET
 import numpy as np
 from numpy.linalg import inv
 import scipy as sp
 from pykalman import KalmanFilter
-import xml.etree.ElementTree as ET
 import pandas as pd
+
+# Importing functions from module Func.py
+from Func import KF_xEvol
+from Func import EKF_PTransitionMatrix
+from Func import KF_MeasurementTransformation
+from Func import EKF_LinearizedMeasurementTransformation
 
 # Importing data into code
 # DV - dataset with Different variance
@@ -17,7 +23,7 @@ DataSV = treeSV.getroot()
 treeOUT = ET.parse('measurements_outlier_samevar.xml')
 DataOUT = treeOUT.getroot()
 
-#Empty array to be used for saving and graphing extimated data
+# Empty array to be used for saving and graphing extimated data
 
 # Constants
 # Gravitational constant
@@ -33,45 +39,11 @@ ts = 0.05
 x_init = np.array([np.arcsin(1), 0])
 P_init = np.eye(2)
 
-#selecting dataset
+# selecting dataset
 Data = DataSV
 
-
-# state space model (x_k = f(x_(k-1) + process noise)
-# x = (a,a')
-# a = angle, a' = angular velocity
-# a_k  = a_(k-1) + a'_(k-1) * ts
-# a'_k = a'_(k-1) - g sin (a_(k-1)) *ts
-# Defining evolution of x
-def fx(x):
-    return np.array([x[0] + x[1]*ts, x[1] - g*ts*np.sin(x[0])])
-
-# for EKF I ll use standart KF with linearized state space model and update it at every step
-# F = df/dx|_(x_k-1) Jakobian derived from f at predicted x_k-1
-# F = (       1           , ts)
-#     (-g*ts*cos(a_(k-1)) , 1 )
-# s is cos of predited value of a_(k-1) from previous step
-def Fx(x):
-    s = np.cos(float(x[0]))
-    return np.array([[1, ts], [-g*ts*s, 1]])
-
-#Process noise
-Q = np.array([[q*pow(ts,3)/3, q*pow(ts,2)/2], [q*pow(ts, 2)/2, q*ts]])
-
-#measurement
-# m = sin (a) + r
-# r = observation noise with variance from dataset at given time
-# defining merurement model
-def hx(x):
-    return np.sin(x[0])
-
-# Here we also linearize the mesurement model
-# H will be the Jakobian for measurment transformation
-# H = (cos(a) , 0)^T
-def Hx (x):
-    s = np.cos(float(x[0]))
-    return np.array([[s, 0]])
-
+# Process noise
+Q = np.array([[q*pow(ts, 3)/3, q*pow(ts, 2)/2], [q*pow(ts, 2)/2, q*ts]])
 
 # Kalman filter
 # Working variables in filter
@@ -87,29 +59,29 @@ P_apri = P_init
 P_post = np.eye(2)
 P_next = np.eye(2)
 
+
 for i in range(101):
     # preparing linearized system for evolution of covariance
-    F = Fx(x_init)
+    F = EKF_PTransitionMatrix(x_init)
     # measurment noise prom data
     r = float(Data[i][2].text)
     R = np.array([[r]])
-    
 
     # prediction step
     # based on evolution of x using system state equations
     # linearized evolution of covariance
-    x_post = fx(x_apri)
+    x_post = KF_xEvol(x_apri)
     P_post = F.dot(P_apri).dot(F.T) + Q
 
     # update step
     # updating Jakobian of mesurement predistion
 
-    H = Hx(x_post)
+    H = EKF_LinearizedMeasurementTransformation(x_post)
     # Kalman gain computation
     K = (P_post.dot(H.T)) @ np.linalg.inv(H.dot(P_post).dot(H.T) + R)
     # Measurements m and predicted measurement h
     m = float(Data[i][1].text)
-    h = hx(x_post)
+    h = KF_MeasurementTransformation(x_post)
 
     # Estimation step
     # Calculating estimated vaues of x and P with Kalman gain and measurements
@@ -117,7 +89,7 @@ for i in range(101):
     P_next = (np.eye(2) - K.dot(H)).dot(P_post)
 
     # Print, save and assign results
-    print('time = ', round(i*ts,3), '    estimated angle = ', round(x_next[0],3), '    estimated angular velocity =', round(x_next[1],3),'    estimated displacement = ', round(np.sin(x_post[0]),3), '    measured displacement = ',round(m,3))
+    print('time = ', round(i*ts, 3), '    estimated angle = ', round(x_next[0], 3), '    estimated angular velocity =', round(
+        x_next[1], 3), '    estimated displacement = ', round(np.sin(x_post[0]), 3), '    measured displacement = ', round(m, 3))
     x_apri = x_next
     P_apri = P_next
-
